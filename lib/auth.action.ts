35e2368f9b078b5b6b3baaaeff1f35ba5765a4de
madhuroshan <madhuroshan2102@gmail.com>
2025-02-08 12:22:37 +0530

    lib/actions folder

---
+++ lib/auth.action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+export const signup = async (
+  name: string,
+  email: string,
+  password: string
+) => {};
+
+export const login = async (email: string, password: string) => {};
+export const currentUser = async () => {};
