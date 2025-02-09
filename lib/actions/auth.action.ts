cbd2796bc1c1a2c7d62ff1235b8b30d8fc761849
madhuroshan <madhuroshan2102@gmail.com>
2025-02-09 14:15:49 +0530

    fix

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,41 +1,43 @@
-'use server'
-import User from '../database/models/user.model'
-import bcryptjs from 'bcryptjs'
-import { createSession } from '../session'
-import { connectToMongoDB } from '../database/mongoose'
+"use server";
+import User from "../database/models/user.model";
+import bcryptjs from "bcryptjs";
+import { createSession } from "../session";
+import { connectToMongoDB } from "../database/mongoose";
 
 export const signup = async (name: string, email: string, password: string) => {
-	try {
-		// Connect to database
-		await connectToMongoDB()
+  try {
+    // Connect to database
+    await connectToMongoDB();
 
-		// Check if the user already exists
-		const existingUser = await User.findOne({
-			email,
-		})
-		// If the user exists, throw an error
-		if (existingUser) {
-			throw new Error('User already exists')
-		}
+    // Check if the user already exists
+    const existingUser = await User.findOne({
+      email,
+    });
+    // If the user exists, throw an error
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
 
-		// Hash the password and store it in the database
-		const hashPassword = bcryptjs.hash(password!, 10)
+    // Hash the password and store it in the database
+    const hashPassword = await bcryptjs.hash(password!, 10);
+    console.log(hashPassword);
 
-		// If the user does not exist, create a new user
-		const newUser = await User.create({
-			name,
-			email,
-			password: hashPassword,
-		})
+    // If the user does not exist, create a new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    console.log(newUser);
 
-		// Create a session for the user and store it in the cookie
-		await createSession(JSON.stringify(newUser._id))
+    // Create a session for the user and store it in the cookie
+    await createSession(JSON.stringify(newUser._id));
 
-		// Redirect the user to the home page
-	} catch (error) {
-		console.log(error)
-	}
-}
+    // Redirect the user to the home page
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const login = async (email: string, password: string) => {}
-export const currentUser = async () => {}
+export const login = async (email: string, password: string) => {};
+export const currentUser = async () => {};
