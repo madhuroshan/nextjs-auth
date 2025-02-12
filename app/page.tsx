900230c023be749a89f3463ccefb887b0468e608
Aditi <aditideshpande2908@gmail.com>
2025-02-12 20:17:16 +0530

    add edit user profile logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import UserProfile from "@/components/UserProfile";
-import { currentUser } from "@/lib/actions/auth.action";
+import UserProfile from '@/components/UserProfile'
+import { currentUser } from '@/lib/actions/auth.action'
+import EditInfo from './editInfo/page'
 
 export default async function Home() {
-  const user = await currentUser();
-  return (
-    <div>
-      <UserProfile user={user as { name: string; email: string }} />
-    </div>
-  );
+	const user = await currentUser()
+	return (
+		<div>
+			<UserProfile
+				user={
+					user as {
+						name: string
+						email: string
+						bio: string
+						company: string
+						website: string
+					}
+				}
+			/>
+			<EditInfo
+				user={
+					user as {
+						name: string
+						email: string
+						bio: string
+						company: string
+						website: string
+					}
+				}
+			/>
+		</div>
+	)
 }
